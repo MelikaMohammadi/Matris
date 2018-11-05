54b59135d34d91280b6ffdb59155f3569d22abed
MelikaMohammadi <44440672+MelikaMohammadi@users.noreply.github.com>
2018-11-05 23:30:37 +0330

    Update Row.java

--- src/Matrix/Row.java
+++ src/Matrix/Row.java
@@ -1,14 +1,15 @@
 package Matrix;
 
 public class Row {
-    int [][]sort(int [][]array,int k ){
+    int [][]sort(int [][]array,int k ,int row){
         for (int i = 0; i < array[k].length; i++) {
                 for (int j = 0; j <(array[k].length)-1;j++) {
                 if (array[k][j + 1] < array[k][j]) {
-
-                    int temp = array[k][j];
-                    array[k][j]= array[k][j+1];
-                    array[k][j+1]= temp;
+                    for (int l = 0; l <row ; l++) {
+                        
+                    int temp = array[l][j];
+                    array[l][j]= array[l][j+1];
+                    array[l][j+1]= temp;}
                 }
             }
         }
