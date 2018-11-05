3fb3d0423b658ccfd6e322fb82cb3fe310e5c5d7
MelikaMohammadi <44440672+MelikaMohammadi@users.noreply.github.com>
2018-11-05 23:29:10 +0330

    Update Column.java

--- src/Matrix/Column.java
+++ src/Matrix/Column.java
@@ -3,15 +3,17 @@ package Matrix;
 import java.util.*;
 
 public class Column {
-    static  int[][] sort(int[][] array, int col,int row) {
+    static  int[][] sort(int[][] array, int col,int Col) {
 
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j <(array.length)-1;j++) {
                 if (array[j + 1][col] < array[j][col]) {
+                    for (int k = 0; k <Col ; k++) {
 
-                    int temp = array[j][col];
-                    array[j][col]= array[j+1][col];
-                    array[j+1][col]= temp;
+
+                    int temp = array[j][k];
+                    array[j][k]= array[j+1][k];
+                    array[j+1][k]= temp;}
                 }
             }
         }
